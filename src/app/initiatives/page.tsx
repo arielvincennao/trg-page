e988978c6f3d3134d018fbc58f9aa0e3839b31a5
Ariel Vincennao <arielvincennao@hotmail.com>
2025-06-23 20:21:29 -0300

    Fixed deploy error

--- src/app/initiatives/page.tsx
+++ src/app/initiatives/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
 import { cardContent } from "../data/cardContent";
